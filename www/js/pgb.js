e1c81ea671d45bbf20d2a60fe293f2da0b3732d5
Marcin Goryl <marcin.goryl@microsoft.wsei.edu.pl>
2017-03-12 16:47:49 +0100

    dfdfd

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -18,3 +18,15 @@ function deviceInfo() {
 
 	document.getElementById("deviceDetails").innerHTML = info;	
 }
+
+function McGorisInfo() {
+
+	info =  'Imie   : '    + 'McGoris' + '<br>' + 
+			'Nazwisko    : '     + 'Goryl' + '<br>' + 
+			'E-mail : '  + 'mcgoris@o2.pl' + '<br>' + 
+			'Strona internetowa: ' + 'mcastronomia.blogspot.com' + '<br>' + 
+			'Kierunek:    : '     + 'informatyka' + '<br>' + 
+			'Semestr : '  + '5' + '<br>';
+
+	document.getElementById("deviceDetails").innerHTML = info;	
+}
