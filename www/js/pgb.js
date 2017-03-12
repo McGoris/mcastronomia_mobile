f29a4fc2e0ece63bc2b82fde226eb38b1dc0a3d4
Marcin Goryl <mcgorispl@gmail.com>
2017-03-12 17:22:07 +0100

    zainteresowania

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -4,19 +4,10 @@ function init() {
 
 function onDeviceReady() {
 	navigator.notification.beep(2);
-	deviceInfo();
+	McGorisInfo();
 }
 
-function deviceInfo() {
-
-	info =  'Device Model   : '    + device.model + '<br>' + 
-			'Device Name    : '     + device.name + '<br>' + 
-			'Device Cordova : '  + device.cordova + '<br>' + 
-			'Device Platform: ' + device.platform + '<br>' + 
-			'Device UUID    : '     + device.uuid + '<br>' + 
-			'Device Version : '  + device.version + '<br>';
-
-	document.getElementById("deviceDetails").innerHTML = info;	
+	
 }
 
 function McGorisInfo() {
