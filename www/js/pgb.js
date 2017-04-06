abfbf8b63ea64f313a0b7ac92f5b9b6211ce7c67
Marcin Goryl <mcgorispl@gmail.com>
2017-04-06 12:06:40 +0200

    i

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -27,18 +27,61 @@ function wypisz_haslo()
 
 window.onload = start;
 
+var litery = new Array(35);
+
+litery[0] = "A";
+litery[1] = "Ą";
+litery[2] = "B";
+litery[3] = "C";
+litery[4] = "Ć";
+litery[5] = "D";
+litery[6] = "E";
+litery[7] = "Ę";
+litery[8] = "F";
+litery[9] = "G";
+litery[10] = "H";
+litery[11] = "I";
+litery[12] = "J";
+litery[13] = "K";
+litery[14] = "L";
+litery[15] = "Ł";
+litery[16] = "M";
+litery[17] = "N";
+litery[18] = "Ń";
+litery[19] = "O";
+litery[20] = "Ó";
+litery[21] = "P";
+litery[22] = "Q";
+litery[23] = "R";
+litery[24] = "S";
+litery[25] = "Ś";
+litery[26] = "T";
+litery[27] = "U";
+litery[28] = "V";
+litery[29] = "W";
+litery[30] = "X";
+litery[31] = "Y";
+litery[32] = "Z";
+litery[33] = "Ż";
+litery[34] = "Ź";
+
+
 function start()
 
 {
-	var div = "";
+	var divy ="";
 	
 	for (i=0; i<=34; i++)
 	{
-		div = div + '<div class="litera">A</div>';
+		divy = divy + '<div class="litera">'+litery[i]+'</div>';
+		if((i+1) % 7 == 0) 
+			{	
+				divy = divy + '<div style="clear:both;"></div>';
+			}
 	}
 	
 	
-	document.getElementById("alfabet").innerHTML = div;
+	document.getElementById("alfabet").innerHTML = divy;
 	
 	wypisz_haslo();
 }
