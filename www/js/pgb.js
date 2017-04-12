bbadea0d3218862761d581a166bfcb4df67c8c3f
Marcin Goryl <mcgorispl@gmail.com>
2017-04-12 22:11:49 +0200

    ikonka

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,9 +1,72 @@
-function OtworzStrone()
-{
-var ref = cordova.InAppBrowser.open('http://mcastronomia.blogspot.com', '_blank', 'location=yes');
-window.open = cordova.InAppBrowser.open;
+var inAppBrowserRef;
+
+function showHelp(url) {
+
+    var target = "_blank";
+
+    var options = "location=yes,hidden=yes";
+
+    inAppBrowserRef = cordova.InAppBrowser.open(url, target, options);
+
+    inAppBrowserRef.addEventListener('loadstart', loadStartCallBack);
+
+    inAppBrowserRef.addEventListener('loadstop', loadStopCallBack);
+
+    inAppBrowserRef.addEventListener('loaderror', loadErrorCallBack);
 
 }
+
+function loadStartCallBack() {
+
+    $('#status-message').text("Trwa ładowanie ...");
+
+}
+
+function loadStopCallBack() {
+
+    if (inAppBrowserRef != undefined) {
+
+        inAppBrowserRef.insertCSS({ code: "body{font-size: 25px;" });
+
+        $('#status-message').text("");
+
+        inAppBrowserRef.show();
+    }
+
+}
+
+function loadErrorCallBack(params) {
+
+    $('#status-message').text("");
+
+    var scriptErrorMesssage =
+       "alert('Przepraszamy. Nie można otworzyć strony. : "
+       + params.message + "');"
+
+    inAppBrowserRef.executeScript({ code: scriptErrorMesssage }, executeScriptCallBack);
+
+    inAppBrowserRef.close();
+
+    inAppBrowserRef = undefined;
+
+}
+
+function executeScriptCallBack(params) {
+
+    if (params[0] == null) {
+
+        $('#status-message').text(
+           "Przepraszamy. Nie można otworzyć strony. : '"
+           + params.message + "'");
+    }
+
+}
+
+function OtworzStrone()
+{
+	ref.addEventListener(eventname, callback);
+}
+
 
 function exitApp()
 {
