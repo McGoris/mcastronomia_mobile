94784b738ad4955795c382294af6cea586a9e5b7
Marcin Goryl <mcgorispl@gmail.com>
2017-04-12 22:16:59 +0200

    ikonka

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,70 +1,7 @@
-var inAppBrowserRef;
-
-function showHelp(url) {
-
-    var target = "_blank";
-
-    var options = "location=yes,hidden=yes";
-
-    inAppBrowserRef = cordova.InAppBrowser.open(url, target, options);
-
-    inAppBrowserRef.addEventListener('loadstart', loadStartCallBack);
-
-    inAppBrowserRef.addEventListener('loadstop', loadStopCallBack);
-
-    inAppBrowserRef.addEventListener('loaderror', loadErrorCallBack);
-
-}
-
-function loadStartCallBack() {
-
-    $('#status-message').text("Trwa ładowanie ...");
-
-}
-
-function loadStopCallBack() {
-
-    if (inAppBrowserRef != undefined) {
-
-        inAppBrowserRef.insertCSS({ code: "body{font-size: 25px;" });
-
-        $('#status-message').text("");
-
-        inAppBrowserRef.show();
-    }
-
-}
-
-function loadErrorCallBack(params) {
-
-    $('#status-message').text("");
-
-    var scriptErrorMesssage =
-       "alert('Przepraszamy. Nie można otworzyć strony. : "
-       + params.message + "');"
-
-    inAppBrowserRef.executeScript({ code: scriptErrorMesssage }, executeScriptCallBack);
-
-    inAppBrowserRef.close();
-
-    inAppBrowserRef = undefined;
-
-}
-
-function executeScriptCallBack(params) {
-
-    if (params[0] == null) {
-
-        $('#status-message').text(
-           "Przepraszamy. Nie można otworzyć strony. : '"
-           + params.message + "'");
-    }
-
-}
-
 function OtworzStrone()
 {
-	ref.addEventListener(eventname, callback);
+	var ref = cordova.InAppBrowser.open('http://apache.org', '_blank', 'location=yes');
+	window.open = cordova.InAppBrowser.open;
 }
 
 
