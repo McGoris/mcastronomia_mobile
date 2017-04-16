e0f51c64cacd42aa0dc54b47f981b9503e8d2d1f
Marcin Goryl <mcgorispl@gmail.com>
2017-04-16 13:02:05 +0200

    quz

--- www/question.js
+++ www/question.js
@@ -1,71 +1,71 @@
 var questions = [{
-	"question": "Ile nóg ma pies z kulawą nogą ?",
-	"option1": "1",
-	"option2": "2",
-	"option3": "4",
-	"option4": "nie ma",
-	"answer": "4"
-}, {
-	"question": "Czy Korwin Krull masakruje lewaków?",
-	"option1": "Owszem",
-	"option2": "Nie",
-	"option3": "Jest feministą, nie odważyłby się",
-	"option4": "Lol",
-	"answer": "4"
-}, {
-	"question": "Ile jąder ma Barrack?",
-	"option1": "jedno",
-	"option2": "Obama",
-	"option3": "Trzy",
-	"option4": "Cztery",
+	"question": "Jaka jest najbliższa nas Galaktyka? ",
+	"option1": "NGC 253",
+	"option2": "Galaktyka Andromedy",
+	"option3": "Galaktyka Barnarda",
+	"option4": "Messier 51",
 	"answer": "2"
 }, {
-	"question": "A",
-	"option1": "B",
-	"option2": "C",
-	"option3": "D",
-	"option4": "E",
-	"answer": "1"
+	"question": "Ile lat trwa precesja osi Ziemi?",
+	"option1": "15000 lat",
+	"option2": "540 lat",
+	"option3": "6730 lat",
+	"option4": "26000 lat",
+	"answer": "4"
 }, {
-	"question": "F ?",
-	"option1": "G",
-	"option2": "H",
-	"option3": "I",
-	"option4": "J",
+	"question": "Kto odkrył księżyce Jowisza?",
+	"option1": "Jan Heweliusz",
+	"option2": "Galileusz",
+	"option3": "William Herschel",
+	"option4": "Isaac Newton",
 	"answer": "2"
 }, {
-	"question": "K",
-	"option1": "L",
-	"option2": "LL",
-	"option3": "M",
-	"option4": "N",
+	"question": "Ile Mars ma księżyców?",
+	"option1": "2",
+	"option2": "5",
+	"option3": "10",
+	"option4": "1",
 	"answer": "1"
 }, {
-	"question": "R ?",
-	"option1": "RR",
-	"option2": "RRR",
-	"option3": "RRRR",
-	"option4": "RRRRR",
-	"answer": "3"
+	"question": "Jak nazywa się nasza galaktyka?",
+	"option1": "Wielki Obłok Magellana",
+	"option2": "Droga Mleczna",
+	"option3": "Leo I",
+	"option4": "Galaktyka Andromedy",
+	"answer": "2"
 }, {
-	"question": "G ?",
-	"option1": "r",
-	"option2": "T",
-	"option3": "D",
-	"option4": "V",
+	"question": "Najjaśniejszą gwiazdą na nocnym niebie jest?",
+	"option1": "Syriusz",
+	"option2": "Gwiazda Polarna",
+	"option3": "Gwiazda Barnarda",
+	"option4": "Alfa Centauri",
 	"answer": "1"
 }, {
-	"question": "F ?",
-	"option1": "f",
-	"option2": "ff",
-	"option3": "fff",
-	"option4": "XA",
+	"question": "Gwiazda Mimosa w gwiazdozbiorze Krzyża Południa wskazuje ?",
+	"option1": "Północ",
+	"option2": "Południe",
+	"option3": "Wschód",
+	"option4": "Zachód",
 	"answer": "3"
 }, {
-	"question": "FG?",
-	"option1": "RF",
-	"option2": "FCV",
-	"option3": "c",
-	"option4": "RFTF",
+	"question": "W jakim gwiazdozbiorze znajduje się biegun południowy Ziemi ?",
+	"option1": "Muchy",
+	"option2": "Oktanta",
+	"option3": "Krzyża Południa",
+	"option4": "Żagla",
+	"answer": "2"
+}, {
+	"question": "Podczas równonocy wiosennej Słońce znajduje się obecnie w: ?",
+	"option1": "Gwiazdozbiorze Panny",
+	"option2": "Gwiazdozbiorze Byka",
+	"option3": "Gwiazdozbiorze Ryb",
+	"option4": "Gwiazdozbiorze Strzelca",
 	"answer": "3"
+}, {
+	"question": "Najgrubsza warstwa atmosfery Ziemi znajduje się?",
+	"option1": "Na równiku",
+	"option2": "Na biegunie północnym",
+	"option3": "Na biegunie południowym",
+	"option4": "Na zwrotnikach Raka i Koziorożca",
+	"answer": "1"
 }];
