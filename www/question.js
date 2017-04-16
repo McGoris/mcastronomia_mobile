71a9eae02249a22333d8ba87b4534e7154c6a44d
Marcin Goryl <mcgorispl@gmail.com>
2017-04-16 15:53:28 +0200

    quz

--- www/question.js
+++ www/question.js
@@ -152,4 +152,137 @@ var questions = [{
 	"option3": "Alderamin",
 	"option4": "Alpheratz",
 	"answer": "3"		
+	
+	
+	
+
+
+
+}, {
+	"question": "Jakie są najbardziej znane galaktyki karłowate, które okrążają Drogę Mleczną?",
+	"option1": "Obłoki Magellana",
+	"option2": "Arp 44",
+	"option3": "Galaktyki w Pannie",
+	"option4": "M81 i M82",
+	"answer": "1"
+}, {
+	"question": "Najbliższą gwiazdą o typie widmowym A jest?",
+	"option1": "Altair",
+	"option2": "Deneb",
+	"option3": "Syriusz",
+	"option4": "Mizar",
+	"answer": "3"
+}, {
+	"question": "Gwiazda Gacrux w gwiazdozbiorze Krzyża Południa wskazuje ?",
+	"option1": "Północ",
+	"option2": "Południe",
+	"option3": "Wschód",
+	"option4": "Zachód",
+	"answer": "1"
+}, {
+	"question": "W jakim gwiazdozbiorze znajduje się obecnie biegun północny Ziemi ?",
+	"option1": "Małej Niedźwiedzicy",
+	"option2": "Oriona",
+	"option3": "Łabędzia",
+	"option4": "Wielkiej Niedźwiedzicy",
+	"answer": "1"
+}, {
+	"question": "Podczas równonocy jesiennej Słońce znajduje się obecnie w: ?",
+	"option1": "Gwiazdozbiorze Lwa",
+	"option2": "Gwiazdozbiorze Byka",
+	"option3": "Gwiazdozbiorze Ryb",
+	"option4": "Gwiazdozbiorze Panny",
+	"answer": "4"
+}, {
+	"question": "Średnica Słońca wynosi:?",
+	"option1": "650 000 km",
+	"option2": "1 392 000 km",
+	"option3": "2 140 000 000 km",
+	"option4": "8 560 000 km",
+	"answer": "2"
+}, {
+	"question": "Jaki typ widmowy mają gwiazdy o najniższej temperaturze powierzchni?",
+	"option1": "A",
+	"option2": "B",
+	"option3": "M",
+	"option4": "O",
+	"answer": "3"
+}, {
+	"question": "Typ widmowy gwiazd cyrkonowych oznaczamy literą?",
+	"option1": "C",
+	"option2": "S",
+	"option3": "F",
+	"option4": "Q",
+	"answer": "2"
+}, {
+	"question": "Jaki typ widmowy ma Słońce?",
+	"option1": "A3V",
+	"option2": "G5III",
+	"option3": "G2V",
+	"option4": "B0IV",
+	"answer": "3"
+}, {
+	"question": "Model z Ziemią w centrum i okrążającymi je planetami i Słońcem to: ?",
+	"option1": "Heliocentryzm",
+	"option2": "Egocentryzm",
+	"option3": "Geocentryzm",
+	"option4": "Epicentryzm",
+	"answer": "3"
+}, {
+	"question": "Najbliższa nam gwiazda(pomijając Słońce) to: ",
+	"option1": "Gliese 581",
+	"option2": "Syriusz",
+	"option3": "Gwiazda Barnarda",
+	"option4": "Alfa Centauri",
+	"answer": "4"
+}, {
+	"question": "W którą stronę przemieszcza się nasz Układ Słoneczny okrążając dookoła centrum Drogi Mlecznej?",
+	"option1": "W stronę gwiazdozbioru Wężownika",
+	"option2": "W stronę gwiazdozbioru Herkulesa",
+	"option3": "W stronę gwiazdozbioru Strzelca",
+	"option4": "W stronę gwiazdozbioru Wolarza",
+	"answer": "2"
+}, {
+	"question": "Mizar, Phecda, Merak, Dubhe - to są gwiazdy",
+	"option1": "z konstelacji Oriona",
+	"option2": "z konstelacji Wielkiego Psa",
+	"option3": "z konstelacji Wielkiej Niedźwiedzicy",
+	"option4": "z konstelacji Kasjopei",
+	"answer": "3"
+}, {
+	"question": "Gwiazda ciągu głównego ma oznaczenie w typie widmowym: ",
+	"option1": "I",
+	"option2": "II",
+	"option3": "IV",
+	"option4": "V",
+	"answer": "4"
+}, { 
+	"question": "Pierwszą planetą od Słońca jest: ?",
+	"option1": "Wenus",
+	"option2": "Merkury",
+	"option3": "Mars",
+	"option4": "Ziemia",
+	"answer": "2"
+}, {
+	"question": "Gwiazda Rigel (najjaśniejsza w Orionie) jest oddalona o ok.: ",
+	"option1": "1200 lat świetlnych",
+	"option2": "800 lat świetlnych",
+	"option3": "200 lat świetlnych",
+	"option4": "86 lat świetlnych",
+	"answer": "2"
+}, {
+	"question": "Najjaśnieszą gwiazdą w gwiazdozbiorze Wagi jest: ?",
+	"option1": "Alfa Librae",
+	"option2": "Alnitak",
+	"option3": "Aldebaran",
+	"option4": "Zubenelshamali",
+	"answer": "4"
+}, {
+	"question": "Najjaśnieszą gwiazdą w konstelacji Perseusza jest: ?",
+	"option1": "Alnilam",
+	"option2": "Dubhe",
+	"option3": "Mirfak",
+	"option4": "Zeta Puppis",
+	"answer": "3"		
+		
 }];
